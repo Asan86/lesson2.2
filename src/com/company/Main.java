62f81bc3707fe2ad4e669ff02e58952e516bbc6a
Asan86 <asanturdukulov86@gmail.com>
2020-01-29 01:02:35 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import sun.management.snmp.jvmmib.JvmRTInputArgsTableMeta;
+
+import java.util.Random;
+public class Main {
+
+    public static void main(String[] args) {
+        Random rand = new Random();
+    for (int i = 0; i < 1; i++) {
+        System.out.println(rand.nextInt(107));
+           }
+    }
+}
